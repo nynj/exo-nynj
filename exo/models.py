0a1085d65720b32cfcb578659dfd222bc83c8bc3
wattbuild-josh-diehl <josh.diehl@wattbuild.com>
2025-06-21 10:04:54 -0400

    Add new `ModelCard` entry for Qwen3-30B-A3B-8bit in `models.py`

--- exo/models.py
+++ exo/models.py
@@ -498,6 +498,13 @@ model_cards: ModelCardCollection = {
       "MLXDynamicShardInferenceEngine": "mlx-community/Qwen3-30B-A3B-4bit-DWQ-0508",
     },
   ),
+  "Qwen3-30B-A3B-8bit": ModelCard(
+    pretty_name="Qwen3-30B-A3B-8bit",
+    layers=48,
+    repo={
+      "MLXDynamicShardInferenceEngine": "mlx-community/Qwen3-30B-A3B-8bit",
+    },
+  ),
   "Qwen3-30B-A3B-bf16": ModelCard(
     pretty_name="Qwen3-30B-A3B-bf16",
     layers=48,
